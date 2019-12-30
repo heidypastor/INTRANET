87a35d4ddad7fcb51e8474d5712881e125010258
Heidy <heidypastor13@gmail.com>
2019-12-30 14:23:36 -0500

    Cambios a Areas.php Documents.php Indicators.php Users.php

--- app/Areas.php
+++ app/Areas.php
@@ -6,5 +6,32 @@ use Illuminate\Database\Eloquent\Model;
 
 class Areas extends Model
 {
-    //
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'AreaName', 'AreaSede', 
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        
+    ];
 }
