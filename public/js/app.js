9b8c34fb3df03e0877dfbc0105f8fa6b208d937b
luis de la hoz <davidoy69@hotmail.com>
2020-01-03 15:39:20 -0500

    npm run dev

--- public/js/app.js
+++ public/js/app.js
@@ -29157,8 +29157,8 @@ window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\laragon\www\intranet\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\laragon\www\intranet\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\laragon\www\INTRANET\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\INTRANET\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
