1954b4f6c33d94491c6ff9121eb7c266bfdc117c
Luis De la hoz <luisdelahoz0@gmail.com>
2020-01-07 00:21:29 -0500

    new npm run prod

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -37,14 +37,14 @@ return [
             'app_id' => env('PUSHER_APP_ID'),
             'options' => [
                 'cluster' => env('PUSHER_APP_CLUSTER'),
-                'encrypted' => true,
                 'host' => '127.0.0.1',
                 'port' => 6001,
-                'scheme' => 'http'
-                // 'useTLS' => false,
+                'scheme' => 'http',
+                'useTLS' => false,
                 // 'wsPort' => 6001,
-                // 'disableStats' => true,
-                // 'wsHost'=> window.location.hostname
+                'disableStats' => false,
+                // 'wsHost'=> window.location.hostname,
+                // 'encrypted' => true,
             ],
         ],
 
