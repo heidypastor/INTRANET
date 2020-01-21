5fa0f21e08fc6438dab79cb89871e965cb379e55
luis de la hoz <davidoy69@hotmail.com>
2020-01-21 16:53:32 -0500

    falta recuperar los plugins del datatables al node-modules

--- resources/views/process/index.blade.php
+++ resources/views/process/index.blade.php
@@ -24,8 +24,8 @@ Procesos
 	</div>
 
 	<div class="card-body">
-		<div class="table-responsive table-upgrade">
-			<table class="table table-compact display" id="tableProcesses">
+		<div class="table-responsive">
+			<table class="table display" id="tableProcesses">
 			  <thead>
 			    <th class="text-center">Nombre</th>
 			    <th class="text-center">Revisión</th>
@@ -76,6 +76,13 @@ Procesos
 		</div>
 	</div>
 @endsection
+
+@push('css')
+        <link href="{{ asset('css') }}/datatable-depen.css" rel="stylesheet"/>
+        <link href="{{ asset('css') }}/datatable-plugins.css" rel="stylesheet"/>
+@endpush
+
+
 {{-- librerias adicionales para el funcionmiento de la vista --}}
 @push('js')
 	<script src="{{ asset('js') }}/datatable-depen.js"></script>
@@ -84,97 +91,59 @@ Procesos
 
 {{-- scripts adicionales para el funcionmiento de la vista --}}
 @push('scripts')
-{{-- <script type="text/javascript">
-    $(document).ready( function () {
-        $('#tableProcesses').DataTable({
-        	"dom": "<'row'<'col-md-3'l><'col-md-5'B><'col-md-4'f>>" +
-        	    "<'row'<'col-md-12'tr>>" +
-        	    "<'row'<'col-md-6'i><'col-md-6'p>>",
-        	"scrollX": true,
-        	"autoWidth": true,
-        	"select": true,
-        	"colReorder": true,
-        	"searchHighlight": true,
-        	"responsive": true,
-        	"keys": true,
-        	"lengthChange": true,
-        	// "buttons": [
-        	//     botoncito,
-        	// ],
-        	"language": {
-        	    "sProcessing":     "Procesando...",
-        	    "sLengthMenu":     "Mostrar _MENU_ registros",
-        	    "sZeroRecords":    "No se encontraron resultados",
-        	    "sEmptyTable":     "Ningún dato disponible en esta tabla",
-        	    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-        	    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-        	    "sInfoFiltered":   "",
-        	    "sInfoPostFix":    "",
-        	    "sSearch":         "Buscar:",
-        	    "sUrl":            "",
-        	    "sInfoThousands":  ",",
-        	    "sLoadingRecords": "Cargando...",
-        	    "oPaginate": {
-        	        "sFirst":    "Primero",
-        	        "sLast":     "Último",
-        	        "sNext":     "Siguiente",
-        	        "sPrevious": "Anterior"
-        	    },
-        	    "oAria": {
-        	        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-        	        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-        	    },
-        	    "colvis": 'Ajouté au presse-papiers',
-        	}
-        });
-    } );
-</script> --}}
-<script type="text/javascript">
-$(document).ready(function() {
 
-
-	/*var botoncito define los botones que se usaran si el usuario es programador*/
-	var botoncito = (true) ? [{extend: 'colvis', text: 'Columnas Visibles'}, {extend: 'copy', text: 'Copiar'}, {extend: 'excel', text: 'Excel'}, {extend: 'pdf', text: 'Pdf'}, {
-					extend: 'collection',
-					text: 'Selector',
-					buttons: ['selectRows', 'selectCells']
-				}] : [{extend: 'colvis', text: 'Columnas Visibles'}, {extend: 'excel', text: 'Excel'}];
-
-	/*funcion para renderizar la tabla de cotizacion.index*/
-	$('#tableProcesses').DataTable({
-		responsive: true,
-		select: true,
-		scrollX:true,
-		dom: 'Bfrtip',
-		buttons: [
-			botoncito, {
-				extend: 'collection',
-				text: 'Selector',
-				buttons: ['selectRows', 'selectCells']
+<script>
+	$(document).ready(function() {
+		// /*var rol defino el rol del usuario*/
+		// var rol = "<?php echo Auth::user()->UsRol; ?>";
+		// /*var botoncito define los botones que se usaran si el usuario es programador*/
+		// var botoncito = (rol == 'Programador') ? [{extend: 'colvis', text: 'Columnas Visibles'}, {extend: 'copy', text: 'Copiar'}, {extend: 'excel', text: 'Excel'}, {extend: 'pdf', text: 'Pdf'}, {
+		// 			extend: 'collection',
+		// 			text: 'Selector',
+		// 			buttons: ['selectRows', 'selectCells']
+		// 		}] : [{extend: 'colvis', text: 'Columnas Visibles'}, {extend: 'excel', text: 'Excel'}];
+		/*inicializacion de datatable general*/        
+		$('.table').DataTable({
+			"dom": "<'row'<'col-md-3'l><'col-md-5'B><'col-md-4'f>>" +
+				"<'row'<'col-md-12'tr>>" +
+				"<'row'<'col-md-6'i><'col-md-6'p>>",
+			"scrollX": false,
+			"autoWidth": true,
+			// "select": true,
+			"colReorder": true,
+			"searchHighlight": true,
+			"responsive": true,
+			"keys": true,
+			"lengthChange": true,
+			"buttons": [
+				// botoncito,
+			],
+			"language": {
+				"sProcessing":     "Procesando...",
+				"sLengthMenu":     "Mostrar _MENU_ registros",
+				"sZeroRecords":    "No se encontraron resultados",
+				"sEmptyTable":     "Ningún dato disponible en esta tabla",
+				"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+				"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+				"sInfoFiltered":   "",
+				"sInfoPostFix":    "",
+				"sSearch":         "Buscar:",
+				"sUrl":            "",
+				"sInfoThousands":  ",",
+				"sLoadingRecords": "Cargando...",
+				"oPaginate": {
+					"sFirst":    "Primero",
+					"sLast":     "Último",
+					"sNext":     "Siguiente",
+					"sPrevious": "Anterior"
+				},
+				"oAria": {
+					"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+					"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+				},
+				"colvis": 'Ajouté au presse-papiers',
 			}
-		],
-		colReorder: true,
-		ordering: true,
-		autoWith: true,
-		searchHighlight: true,
-		columnDefs: [{
-			"targets": 15,
-			"data": "id",
-			"render": function(data, type, row, meta) {
-				return "<a method='get' href='/procesos/" + data + "/' class='btn btn-primary btn-block'>Ver mas</a>";
-			}
-		}]
+		});
 	});
-
-	/*funcion para resaltar las busquedas*/
-	var table = $('#tableProcesses').DataTable();
-
-	table.on('draw', function() {
-		var body = $(table.table().body());
-		body.unhighlight();
-		body.highlight(table.search());
-	});
-});
-
 </script>
 @endpush
