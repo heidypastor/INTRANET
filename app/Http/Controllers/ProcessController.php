88ee835a42cf7fc83d2ccddf380f687039660072
luis de la hoz <davidoy69@hotmail.com>
2020-03-11 09:54:03 -0500

    se añaden las consultas de elementos relacionados a la vista

--- app/Http/Controllers/ProcessController.php
+++ app/Http/Controllers/ProcessController.php
@@ -56,23 +56,20 @@ class ProcessController extends Controller
             $soportes = Process::all(['id', 'ProcName']);
             $seguimientos = Seguimiento::all(['id', 'SeguiName']);
 
+            /* variables para los formularios de destroy */
+            $salidasDrop = Output::doesntHave('procesos')->get();
+            $entradasDrop = Input::doesntHave('procesos')->get();
+            $actividadesDrop = Activity::doesntHave('procesos')->get();
+            $seguimientosDrop = Seguimiento::doesntHave('procesos')->get();
+
+            $usuario = Auth::user()->id;
+
+
             /*return $actividades;*/
-            return view('process.create', compact(['roles', 'requisitos', 'documentos', 'entradas', 'salidas', 'actividades', 'indicadores', 'soportes', 'areas', 'seguimientos']));
+            return view('process.create', compact(['roles', 'requisitos', 'documentos', 'entradas', 'salidas', 'actividades', 'indicadores', 'soportes', 'areas', 'seguimientos', 'salidasDrop', 'entradasDrop', 'actividadesDrop', 'seguimientosDrop', 'usuario']));
         }else{
             abort(403, 'El usuario no se encuentra autorizado para crear Procesos');
         }
-
-        /* variables para los formularios de destroy */
-        $salidasDrop = Output::doesntHave('procesos')->get();
-        $entradasDrop = Input::doesntHave('procesos')->get();
-        $actividadesDrop = Activity::doesntHave('procesos')->get();
-        $seguimientosDrop = Seguimiento::doesntHave('procesos')->get();
-
-        $usuario = Auth::user()->id;
-        
-        // return $seguimientosDrop;
-        return view('process.create', compact(['roles', 'requisitos', 'documentos', 'entradas', 'salidas', 'actividades', 'indicadores', 'soportes', 'areas', 'seguimientos', 'salidasDrop', 'entradasDrop', 'actividadesDrop', 'seguimientosDrop', 'usuario']));
-
     }
 
     /**
