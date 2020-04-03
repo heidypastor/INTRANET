83b0974310546c9005a6796fd698df004f406673
Heidy <heidypastor13@gmail.com>
2020-04-03 17:06:41 -0500

    Validación de la creación edición y eliminación de usuarios

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -22,7 +22,7 @@ class UserController extends Controller
     public function index(User $users)
     {
         $users = User::with('roles')->get();
-
+        /*return $users;*/
         return view('users.index', ['users' => $users]);
     }
 
