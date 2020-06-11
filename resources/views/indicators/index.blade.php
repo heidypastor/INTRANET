bf7891aacac007635d76dc5553cc69fd6aa0e61e
Luis De la hoz <luisdelahoz0@gmail.com>
2020-06-10 19:39:04 -0500

    se ajusta el tamaño de las imagenes para maximizar el espacio utilizado

--- resources/views/indicators/index.blade.php
+++ resources/views/indicators/index.blade.php
@@ -9,7 +9,7 @@ Indicadores Estrategicos
 @endsection
 
 @section('content')
-	<div class="col-md-12">
+	<div class="row">
 		<div class="col-md-12 text-center">
 			<h3><strong>Indicadores Estrategicos</strong></h3>
 		</div>
@@ -18,19 +18,21 @@ Indicadores Estrategicos
 		</div>
 		@include('alerts.success')
 	</div>
-	<div class="col-md-12 row">
+	<div class="row">
 		@foreach($Indicators as $indicator)
 			@if($indicator->IndType === 0)
-			<div class="card bg-transparent text-white col-md-5 col-sm-12 text-center index-indicators-1">
-				@if($indicator->IndGraphic === "")
-					<img src="/white/img/graficos1.jpg" class="card-img" alt="Imagen no disponible">
-				@else
-					<img src="{{Storage::url($indicator->IndGraphic)}}" class="card-img" alt="Imagen no disponible">
-				@endif
-				<div class="card-img-overlay">
-					<h5 class="card-title"><strong><a method='GET' href="indicators/{{$indicator->id}}" class="btn btn-sm btn-secondary"> {{$indicator->IndName}}</a></strong></h5>
-					{{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-					<p class="card-text">Last updated 3 mins ago</p> --}}
+			<div class="col-md-6 col-sm-12">
+				<div class="card bg-transparent text-white text-center index-indicators-1">
+					@if($indicator->IndGraphic === "")
+						<img src="/white/img/graficos1.jpg" class="card-img" alt="Imagen no disponible">
+					@else
+						<img src="{{Storage::url($indicator->IndGraphic)}}" class="card-img" alt="Imagen no disponible">
+					@endif
+					<div class="card-img-overlay">
+						<h5 class="card-title"><strong><a method='GET' href="indicators/{{$indicator->id}}" class="btn btn-sm btn-secondary"> {{$indicator->IndName}}</a></strong></h5>
+						{{-- <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+						<p class="card-text">Last updated 3 mins ago</p> --}}
+					</div>
 				</div>
 			</div>
 			@endif
