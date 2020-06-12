446c9531014c9e4225cebdf0867fab83c1e99bd4
Luis De la hoz <luisdelahoz0@gmail.com>
2020-06-12 17:39:40 -0500

    se actualiza la descripcion del label para el campo objetivos en el edit de indicadores

--- resources/views/indicators/edit.blade.php
+++ resources/views/indicators/edit.blade.php
@@ -23,11 +23,6 @@ Indicadores
                 <input name="IndName" type="text" value="{{ old('IndName') ? old('IndName') : $indicator->IndName}}" class="text-center form-control form-control-alternative{{ $errors->has('IndName') ? ' is-invalid' : '' }}">
                 @include('alerts.feedback', ['field' => 'IndName'])
               </div>
-              {{-- <div class="form-group{{ $errors->has('IndObjective') ? ' has-danger' : '' }}">
-                <label>Objetivo del Indicador</label>
-                <input name="IndObjective" type="text" value="{{$indicator->IndObjective}}" class="text-center form-control form-control-alternative{{ $errors->has('IndObjective') ? ' is-invalid' : '' }}">
-                @include('alerts.feedback', ['field' => 'IndObjective'])
-              </div> --}}
               <div class="form-group{{ $errors->has('IndFrecuencia') ? ' has-danger' : '' }}">
                 <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Frecuencia del Indicador</b>" data-content="Seleccione la frecuencia del indicador"><i class="far fa-question-circle"></i> Frecuencia del Indicador</label>
                 <select class="text-center form-control form-control-alternative{{ $errors->has('IndFrecuencia') ? ' is-invalid' : '' }}" required="" name="IndFrecuencia" id="IndFrecuencia">
@@ -38,15 +33,11 @@ Indicadores
                 @include('alerts.feedback', ['field' => 'IndFrecuencia'])
               </div>
               <div class="form-group{{ $errors->has('IndObjective') ? ' has-danger' : '' }}">
-                <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Objetivo del Indicador</b>" data-content="Seleccione el objetivo del indicador de acuerdo a los objetivos presentados en la sección de Nosotros  <ul>
-                  <li>Objetivo 1 - Pólitica de Seguridad y Salud en el Trabajo</li>
-                  <li>Objetivo 2 - Pólitica de Calidad</li>
-                  <li>Objetivo 3 - Pólitica Ambiental</li>
-                </ul>"><i class="far fa-question-circle"></i> Objetivo del Indicador</label>
+                <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Objetivo del Indicador</b>" data-content="Seleccione el objetivo del indicador de acuerdo a los objetivos presentados en la sección de Nosotros <br><br><ul><li>Objetivo 1 - Pólitica de Seguridad y Salud en el Trabajo</li><li>Objetivo 2 - Pólitica de Calidad</li><li>Objetivo 3 - Pólitica Ambiental</li></ul><br><p>Ver sección <strong>Nosotros</strong> en la página de inicio de la intranet para más detalles sobre los objetivos corporativos<ul>"><i class="far fa-question-circle"></i> Objetivo del Indicador</label>
                 <select class="text-center form-control form-control-alternative{{ $errors->has('IndEfe') ? ' is-invalid' : '' }}" required="" name="IndObjective" id="IndObjective">
-                  <option {{ old('IndObjective') == "1" ? "selected" : ($indicator->IndObjective == "1" && !old('IndObjective') ? "selected" : "") }} value="1">Objetivo 1 -> Implementar actividades de promoción y prevención en salud, dirigidas a nuestros trabajadores y de seguridad para nuestros colaboradores, contratistas y visitantes con el fin de prevenir accidentes y enfermedades laborales. </option>
-                  <option {{ old('IndObjective') == "2" ? "selected" : ($indicator->IndObjective == "2" && !old('IndObjective') ? "selected" : "") }} value="2">Objetivo 2 -> Garantizar que los servicios de recolección, transporte, manejo, tratamiento, incineración y destrucción de toda clase de desechos y residuos sean oportunos, adecuados y seguro, previniendo la contaminación y la disminuyendo los impactos que se puedan generar a los recursos naturales </option>
-                  <option {{ old('IndObjective') == "3" ? "selected" : ($indicator->IndObjective == "3" && !old('IndObjective') ? "selected" : "") }} value="3">Objetivo 3 -> Cumplir con los estándares de calidad en la prestación del servicio a nuestros clientes, optimizando y mejorando continuamente en los procesos y procedimientos establecidos en la Empresa, llegando a los estándares de eficiencia, eficacia, efectividad, cumpliendo siempre con la legislación Ambiental Colombiana y los requerimientos de nuestros Clientes. </option>
+                  <option {{ old('IndObjective') == "1" ? "selected" : ($indicator->IndObjective == "1" && !old('IndObjective') ? "selected" : "") }} value="1">Objetivo 1 - Pólitica de Seguridad y Salud en el Trabajo</option>
+                  <option {{ old('IndObjective') == "2" ? "selected" : ($indicator->IndObjective == "2" && !old('IndObjective') ? "selected" : "") }} value="2">Objetivo 2 - Pólitica de Calidad</option>
+                  <option {{ old('IndObjective') == "3" ? "selected" : ($indicator->IndObjective == "3" && !old('IndObjective') ? "selected" : "") }} value="3">Objetivo 3 - Pólitica Ambiental</option>
                 </select>
                 @include('alerts.feedback', ['field' => 'IndObjective'])
               </div>
@@ -85,16 +76,6 @@ Indicadores
                 <input name="IndTable" type="file" class="form-control form-control-alternative{{ $errors->has('IndTable') ? ' is-invalid' : '' }}">
                 @include('alerts.feedback', ['field' => 'IndTable'])
               </div>
-              {{-- <div class="form-group{{ $errors->has('IndDateFrom') ? ' has-danger' : '' }}">
-                <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Fecha Inicio</b>" data-content="Fecha de inicio de toma de datos para el desarrollo del indicador."><i class="far fa-question-circle"></i> Fecha Inicio</label>
-                <input name="IndDateFrom" type="date" value="{{$indicator->IndDateFrom}}" class="text-center form-control form-control-alternative{{ $errors->has('IndDateFrom') ? ' is-invalid' : '' }}">
-                @include('alerts.feedback', ['field' => 'IndDateFrom'])
-              </div>
-              <div class="form-group{{ $errors->has('IndDateUntil') ? ' has-danger' : '' }}">
-                <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Fecha Fin</b>" data-content="Fecha de la última toma de datos para el desarrollo de los indicadores."><i class="far fa-question-circle"></i> Fecha Fin</label>
-                <input name="IndDateUntil" type="date" value="{{$indicator->IndDateUntil}}" class="text-center form-control form-control-alternative{{ $errors->has('IndDateUntil') ? ' is-invalid' : '' }}">
-                @include('alerts.feedback', ['field' => 'IndDateUntil'])
-              </div> --}}
               <div class="form-group{{ $errors->has('IndType') ? ' has-danger' : '' }}">
                 <label data-placement="auto" data-trigger="hover" data-html="true" data-toggle="popover" title="<b>Tipo de Indicador</b>" data-content="Ingresar el tipo de indicador si es de tipo estrategico o general."><i class="far fa-question-circle"></i> Tipo de Indicador</label>
                 <select class="text-center form-control form-control-alternative{{ $errors->has('IndType') ? ' is-invalid' : '' }}" required="" name="IndType" id="IndType">
